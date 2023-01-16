456e79d5f2fe32de2d40bb05843f2e3d4d2f4649
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-01-16 23:32:35 +0100

    Add character name and last location

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,10 +4,19 @@ const Cards = ({ results }) => {
   let display;
   if (results) {
     display = results.map((x) => {
-      let { id, name } = x;
+      let { id, name, image, location } = x;
       return (
         <div key={id} className="col-4">
-          {name}
+          <div className="">
+            <img src={image} alt="" className="img-fluid" />
+            <div className="content">
+              <div className="fs-4 fw-bold mb-4">{name}</div>
+              <div className="">
+                <div className="fs-6">Last location</div>
+                <div className="">{location.name}</div>
+              </div>
+            </div>
+          </div>
         </div>
       );
     });
