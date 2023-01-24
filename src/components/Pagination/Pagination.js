95da1959846b726b7f44e23fa03bb743e6c614cf
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-01-24 23:46:37 +0100

    Add pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,24 +1,18 @@
 import React from "react";
+import ReactPaginate from "react-paginate";
 
-const Pagination = ({ pageNumber, setPageNumber }) => {
-  let next = () => {
-    setPageNumber((x) => x + 1);
-  };
-
-  let prev = () => {
-    if (pageNumber === 1) return;
-    setPageNumber((x) => x - 1);
-  };
-
+const Pagination = ({ info, pageNumber, setPageNumber }) => {
   return (
-    <div className="container d-flex justify-content-center gap-5 my-5">
-      <button onClick={prev} className="btn btn-primary">
-        Previous
-      </button>
-      <button onClick={next} className="btn btn-primary">
-        Next
-      </button>
-    </div>
+    <ReactPaginate
+      className="pagination justify-content-center gap-4 my-4"
+      nextLabel="Next"
+      previousLabel="Prev"
+      nextClassName="btn btn-primary"
+      previousClassName="btn btn-primary"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      pageCount={info?.pages}
+    />
   );
 };
 
