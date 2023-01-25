aba9979f0fef1ed2f4d16baf49ff47ff979a9265
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-01-25 23:52:52 +0100

    Add transition pagination and highlight

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -5,12 +5,17 @@ const Pagination = ({ info, pageNumber, setPageNumber }) => {
   return (
     <ReactPaginate
       className="pagination justify-content-center gap-4 my-4"
+      forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
       nextLabel="Next"
       previousLabel="Prev"
       nextClassName="btn btn-primary"
       previousClassName="btn btn-primary"
+      activeClassName="active"
       pageClassName="page-item"
       pageLinkClassName="page-link"
+      onPageChange={(data) => {
+        setPageNumber(data.selected + 1);
+      }}
       pageCount={info?.pages}
     />
   );
