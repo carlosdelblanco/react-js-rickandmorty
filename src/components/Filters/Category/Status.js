dcceb435f02b2a153f4033e708ac9af16c8fcaaf
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-01-28 23:53:43 +0100

    Add more features

--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -12,7 +12,7 @@ const Status = () => {
           aria-expanded="false"
           aria-controls="collapseThree"
         >
-          Accordion Item #3
+          Status
         </button>
       </h2>
       <div
@@ -21,16 +21,7 @@ const Status = () => {
         aria-labelledby="headingThree"
         data-bs-parent="#accordionExample"
       >
-        <div className="accordion-body">
-          <strong>This is the third item's accordion body.</strong> It is hidden
-          by default, until the collapse plugin adds the appropriate classNamees
-          that we use to style each element. These classNamees control the
-          overall appearance, as well as the showing and hiding via CSS
-          transitions. You can modify any of this with custom CSS or overriding
-          our default variables. It's also worth noting that just about any HTML
-          can go within the <code>.accordion-body</code>, though the transition
-          does limit overflow.
-        </div>
+        <div className="accordion-body"></div>
       </div>
     </div>
   );
