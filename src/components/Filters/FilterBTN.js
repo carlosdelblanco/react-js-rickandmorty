3506bb7d57fb352fbcc3e49ec3b243e5639f02b4
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-01-30 19:58:45 +0100

    Add status filter

--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -1,32 +1,18 @@
 import React from "react";
 
-const FilterBTN = () => {
+const FilterBTN = ({ name, index, items }) => {
   return (
-    <>
-      <div className="form-check">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="flexRadioDefault"
-          id="flexRadioDefault1"
-        />
-        <label class="btn btn-outline-primary" for="flexRadioDefault1">
-          Single toggle
-        </label>
-      </div>
-      <div className="form-check">
-        <input
-          class="form-check-input"
-          type="radio"
-          name="flexRadioDefault"
-          id="flexRadioDefault2"
-          checked
-        />
-        <label className="btn btn-outline-primary" for="flexRadioDefault2">
-          Default checked radio
-        </label>
-      </div>
-    </>
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="radio"
+        name={name}
+        id={`${name}-${index}`}
+      />
+      <label class="btn btn-outline-primary" for={`${name}-${index}`}>
+        {items}
+      </label>
+    </div>
   );
 };
 
