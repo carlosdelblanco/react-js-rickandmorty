9eacda0a8282820430ed29876757c30f7e333826
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-02-12 21:23:59 +0100

    Add more capabilities

--- src/components/Cards/CardDetails.js
+++ src/components/Cards/CardDetails.js
@@ -1,9 +1,27 @@
-import React from "react";
+/* eslint-disable no-restricted-globals */
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const CardDetails = () => {
   let { id } = useParams();
-  return <div>the details from the card are here - {id}</div>;
+  let [fetchedData, updateFetchedData] = useState([]);
+
+  let api = `https://rickandmortyapi.com/api/character/${id}`;
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
+  return (
+    <div className="container d-flex justify-content-center">
+      <div className="">
+        <h1 className="">{name}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default CardDetails;
